b683bfa6bb7afc8c0ae7866939125eba3a1fe924
Kamil Kowalski <kowalski.tychy@gmail.com>
2016-12-07 00:40:07 +0100

    Add Roboto font, disable selection and add home animation

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,16 +1,13 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 import styles from './Home.css';
-
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <div className={styles.container}>
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+      <div className={styles.container}>
+        <div className={styles.greeter}>
+          <h2>Good morning</h2>
         </div>
       </div>
     );
